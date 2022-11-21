bc971a57267f6afaa1454b73b82a43489d42f5be
Dav's <71350166+DaviBaechtold@users.noreply.github.com>
2022-11-21 08:38:57 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,28 @@
+#Davi Baechtold Campos
+#Trabalho 1 - Calculadora RPN 
+
+"""
+Você deverá desenvolver um programa, na sua linguagem de programação favorita, que leia uma string de entrada e execute a operação matemática contida nesta string. Esta string irá representar operações matemáticas segundo uma variação da RPN, que inclui obrigatoriamente parênteses para limitar as operações, conforme exemplos a seguir: 
+
+a) (2 3 +) = 5 
+b) (3 4 *) = 12 
+c) (4 2 2 /) = 1 
+d) ((4 2 +) 3 *) = 18  
+
+O seu programa deverá ser capaz de realizar as seguintes operações:  
+• Adição representada por +; 
+• Subtração representada por -; 
+• Multiplicação representada por *; 
+• Divisão representada por /; 
+
+As operações devem ser realizadas com número reais, mesmo que todos os exemplos aqui utilizados sejam com números inteiros. As operações podem ser aninhadas, sem qualquer limite, 
+como pode ser visto nos exemplos a seguir:  
+• ((3 4 +) (4 2 /) *) = 14 
+• ((3 4 +) (4 (1 1 +) /) *) = 14 
+"""
+
+
+
 import re,time
 
 
